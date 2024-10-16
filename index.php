ea27b34853441166cfb975dc97850cb062cc1fce
grcke <grcwongke@gmail.com>
2024-10-16 19:36:35 +0800

    added index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    <div class="button-container">
+        <h1>HuanFitnessPal</h1>
+        <a href="login.php">Login</a><br>
+        <a href="signup.php">Register</a>
+    </div>
+</body>
+</html>
