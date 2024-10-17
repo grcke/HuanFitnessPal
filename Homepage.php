92bea160479b376953f02928a0ffcf20ec4acbd5
KelvinTSJ <143151478+KelvinTSJ@users.noreply.github.com>
2024-10-18 01:06:37 +0800

    Add files via upload
    Heavy WIP; will add as time goes on

---
+++ Homepage.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<style>
+.header{
+	width: 100%;
+	text-align: center;
+	color: white; 
+	background: #2273f5;
+	border-style: ridge;
+}
+</style>
+
+<div class="header">
+	<h1>Huan Fitness Pal</h1>
+	<h4>For all your fitness needs...</h4>
+</div>
+</html>
