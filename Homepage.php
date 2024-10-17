cf7ed942f75282f4d3fa608d22bbca3df1159efb
KelvinTSJ <143151478+KelvinTSJ@users.noreply.github.com>
2024-10-18 01:30:13 +0800

    Update Homepage.php

--- Homepage.php
+++ Homepage.php
@@ -1,19 +1,40 @@
 <!DOCTYPE HTML>
 <html>
 <style>
-.header{
+header{
 	width: 100%;
 	text-align: center;
 	color: white; 
 	background: #2273f5;
 	border-style: ridge;
+	padding: 30px;
+}
+
+body{
+	background-image: url('https://st4.depositphotos.com/1022135/25748/i/450/depositphotos_257486682-stock-photo-group-young-people-sportswear-talking.jpg');
+	background-size: cover;
+}
+
+.login{
+	width: 40%;
+	background: #c4d7f5;
+	color: black;
+	text-align: center;
+	padding: 30px;
+	margin: 0 auto;
 }
 </style>
 
-<div class="header">
+<header>
 	<h1>Huan Fitness Pal</h1>
 	<h4>For all your fitness needs...</h4>
+</header>
+
+<body>	
+
+<div class="login">
+	<?php include 'login.php';?>
 </div>
 
-<?php include 'index.php';?>
+</body>
 </html>
