63283aa85b3e0fd1ed0385a4b8a2c52ccca18b0f
PRSXFENG <chootingfeng1312@gmail.com>
2024-10-26 20:19:56 +0800

    db format changes

--- hfp.sql
+++ hfp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 17, 2024 at 12:30 PM
+-- Generation Time: Oct 26, 2024 at 02:19 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,19 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `exercise`
+--
+
+CREATE TABLE `exercise` (
+  `RecordID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `DateTime` datetime NOT NULL,
+  `record` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `userinfo`
 --
 
@@ -35,8 +48,56 @@ CREATE TABLE `userinfo` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `userinfo`
+--
+
+INSERT INTO `userinfo` (`userID`, `type`, `email`, `password`) VALUES
+(1, 'user', 'testuser@example.com', '$2y$10$Hbsl5CybJeAenKaLY1wrJe.KUqcZwnxtcj6LqLtAsklNhDoFzwLFu'),
+(2, 'admin', 'testadmin@example.com', '$2y$10$440/2O3hYSFxit5y7y3NzOoNKMRrit1.IUKVYV.5Esk32kloVQj0a');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `water`
+--
+
+CREATE TABLE `water` (
+  `RecordID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `DateTime` datetime NOT NULL,
+  `ammount` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `weight`
+--
+
+CREATE TABLE `weight` (
+  `RecordID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `Date` date NOT NULL,
+  `Weight` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `weight`
+--
+
+INSERT INTO `weight` (`RecordID`, `UserID`, `Date`, `Weight`) VALUES
+(1, 1, '2024-10-26', 88);
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `exercise`
+--
+ALTER TABLE `exercise`
+  ADD PRIMARY KEY (`RecordID`),
+  ADD KEY `UserID` (`UserID`);
 
 --
 -- Indexes for table `userinfo`
@@ -46,14 +107,68 @@ ALTER TABLE `userinfo`
   ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Indexes for table `water`
+--
+ALTER TABLE `water`
+  ADD PRIMARY KEY (`RecordID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `weight`
+--
+ALTER TABLE `weight`
+  ADD PRIMARY KEY (`RecordID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `exercise`
+--
+ALTER TABLE `exercise`
+  MODIFY `RecordID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `userinfo`
 --
 ALTER TABLE `userinfo`
-  MODIFY `userID` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `userID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `water`
+--
+ALTER TABLE `water`
+  MODIFY `RecordID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `weight`
+--
+ALTER TABLE `weight`
+  MODIFY `RecordID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `exercise`
+--
+ALTER TABLE `exercise`
+  ADD CONSTRAINT `exercise_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `userinfo` (`userID`);
+
+--
+-- Constraints for table `water`
+--
+ALTER TABLE `water`
+  ADD CONSTRAINT `water_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `userinfo` (`userID`);
+
+--
+-- Constraints for table `weight`
+--
+ALTER TABLE `weight`
+  ADD CONSTRAINT `weight_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `userinfo` (`userID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
