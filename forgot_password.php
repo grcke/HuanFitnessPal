4b0ba571cd560ecc1894b0c6e5176dbcb3922b67
KelvinTSJ <143151478+KelvinTSJ@users.noreply.github.com>
2024-10-26 18:34:24 +0800

    Update forgot_password.php

--- forgot_password.php
+++ forgot_password.php
@@ -1,21 +1,78 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Forgot Password</title>
-    <style>
-        /* for invalid and valid password criteria */
-        .invalid {
-            color: gray;
-        }
-        .valid {
-            color: green;
-        }
-    </style>
-</head>
+<!DOCTYPE HTML>
+<html>
+<style>
+
+header{
+	right: 0;
+	left: 0;
+	text-align: center;
+	color: white; 
+	background: #405dde;
+	border-style: ridge;
+	margin: 0 auto;
+	padding-top: 20px;
+	padding-bottom: 20px;
+	margin-bottom: 20px;
+}
+
+body{
+	background-image: url('https://st4.depositphotos.com/1022135/25748/i/450/depositphotos_257486682-stock-photo-group-young-people-sportswear-talking.jpg');
+	background-size: cover;
+	margin: 0;
+	right: 0;
+	left: 0;
+}
+
+.forgot{
+	width: 40%;
+	height: 250px;
+	background: #c4d7f5;
+	color: black;
+	text-align: center;
+	padding: 70px;
+	margin: 0 auto;
+	font-size: 120%;
+	border-style: ridge;
+}
+
+input{
+	height: 30px;
+	width: 210px;
+}
+
+.invalid {
+	color: gray;
+}
+.valid {
+	color: green;
+}
+
+footer{
+	right: 0;
+	left: 0;
+	text-align: center;
+	color: white; 
+	background: #405dde;
+	border-style: ridge;
+	margin: 0 auto;
+	padding-top: 20px;
+	padding-bottom: 20px;
+	position: fixed;
+	bottom: 0;
+	border-style: ridge;
+}
+</style>
+
 <body>
-    <h1>Forgot Password</h1>
+
+<header>
+	<h1 style="font-size: 40px">Huan Fitness Pal</h1>
+	<h4 style="font-size: 20px"><i>For all your fitness needs...</i></h4>
+</header>
+
+<div class="forgot">
+    <h1 style="font-size: 35px">Forgot Password?</h1>
+	<h4 style="font-size: 20px">Please input your email address.</h4>
 
     <?php
     session_start();
@@ -148,5 +205,14 @@
         <?php endif; ?>
     
     </script>
+</div>
+
+<footer>
+	<h4>
+	<i>Huan Sdn. Bhd</i><br>
+	<i>All Rights Reserved&copy</i>
+	</h4>
+</footer>
+
 </body>
 </html>
